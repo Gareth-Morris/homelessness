bffdee1119b5829d423dd59070e8f8ef171b9d1c
Gareth Morris <68757194+Gareth-Morris@users.noreply.github.com>
2022-03-22 09:56:08 +0000

    Debug h2 width

--- wp-content/themes/skillcrushstarter/front-page.php
+++ wp-content/themes/skillcrushstarter/front-page.php
@@ -1,5 +1,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,700&display=swap" rel="stylesheet">
 
 <?php
 /**
